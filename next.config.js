a64bf5f050f72cd3a868be4e6bf0b397dc1745b1
Amirsam Sabouri <amirsamsa672@gmail.com>
2026-01-07 09:58:29 +0330

    Fix Netlify bundle size issue

--- next.config.js
+++ next.config.js
@@ -5,11 +5,29 @@ const nextConfig = {
       bodySizeLimit: '10mb',
     },
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias.canvas = false
+    
+    // Reduce bundle size for serverless
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push({
+        'sharp': 'commonjs sharp',
+        'pdfjs-dist': 'commonjs pdfjs-dist',
+      })
+    }
+    
     return config
+  },
+  // Optimize for serverless deployment
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild/linux-x64',
+      'node_modules/sharp/vendor',
+    ],
   },
 }
 
 module.exports = nextConfig
-
